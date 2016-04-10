e453b04ba04f6fd8d9961f3a90a132ecce4f5fc3
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-04-09 21:22:58 -0400

    Create Interpreter.java

---
+++ src/core/Interpreter.java
@@ -0,0 +1,24 @@
+package core;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Interpreter {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		for (;;) {
+			System.out.print("Phigs> ");
+			String line = keyboard.nextLine();
+			if (line.equals("quit")) {
+				break;
+			}
+			TokenList prog = Parser.parseString(null, line);
+			ArrayList<Prim> res = prog.eval();
+			for (Prim p : res) {
+				System.out.println(p);
+			}
+		}
+		keyboard.close();
+	}
+
+}
