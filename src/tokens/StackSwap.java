99e4c2afa5e66d1aedb58d89ba08fc25d2e8b8a8
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-04-09 21:25:15 -0400

    Create StackSwap.java

---
+++ src/tokens/StackSwap.java
@@ -0,0 +1,27 @@
+package tokens;
+
+import java.util.ArrayList;
+
+import core.Prim;
+import core.Token;
+
+public class StackSwap extends Token {
+
+	@Override
+	public void eval(ArrayList<Prim> args) {
+		if(canEval(args)){
+			Prim a = args.remove(0);
+			Prim b = args.remove(0);
+			args.add(0, a);
+			args.add(0, b);
+		}
+		
+	}
+
+	@Override
+	public boolean canEval(ArrayList<Prim> args) {
+		return args.size() >= 2;
+	}
+
+
+}
