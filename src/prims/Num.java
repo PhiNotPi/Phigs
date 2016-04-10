89cd98571819f020878d92838da18c93846484fc
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-04-09 21:50:50 -0400

    Added files via upload

--- src/prims/Num.java
+++ src/prims/Num.java
@@ -8,24 +8,24 @@ import core.Prim;
 
 public class Num extends Prim {
 
-	BigDecimal val;
-	public Num(BigDecimal t) {
-		this.val = t;
-	}
-	public Num(String t) {
-		this.val = new BigDecimal(t);
-	}
+  BigDecimal val;
+  public Num(BigDecimal t) {
+    this.val = t;
+  }
+  public Num(String t) {
+    this.val = new BigDecimal(t);
+  }
 
-	@Override
-	public BigDecimal getNum() {
-		if(val != null){
-			return val;
-		}
-		return new BigDecimal("0");
-	}
-	
-	public String toString(){
-		return this.getNum().round(new MathContext(30)).stripTrailingZeros().toPlainString();
-	}
+  @Override
+  public BigDecimal getNum() {
+    if(val != null){
+      return val;
+    }
+    return new BigDecimal("0");
+  }
+
+  public String toString(){
+    return this.getNum().round(new MathContext(30)).stripTrailingZeros().toPlainString();
+  }
 
 }
